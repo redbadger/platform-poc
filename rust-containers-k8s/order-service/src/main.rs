6aeaed00ff916f62e5f948d241bd903b27425dc9
Pedro Martin <pataruco@gmail.com>
2023-12-19 13:07:54 +0000

    Add .env example for order-service

--- rust-containers-k8s/order-service/src/main.rs
+++ rust-containers-k8s/order-service/src/main.rs
@@ -1,20 +1,14 @@
-// use dotenv::dotenv;
-// use order_service::{api::server, config::Config};
+use dotenv::dotenv;
+use order_service::{api::server, config::Config};
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     dotenv().ok();
-//     let config = Config::new()?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let config = Config::new()?;
 
-//     println!("{:?}", config);
+    println!("{:?}", config);
 
-//     server::create(config).await;
-//     Ok(())
-// }
-
-use std::{thread, time::Duration};
-
-fn main() {
-    println!("Order service started");
-    thread::sleep(Duration::from_secs(60 * 60 * 24));
+    server::create(config).await;
+    Ok(())
 }
+
