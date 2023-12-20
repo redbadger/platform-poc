cc9f17eedab30890522fd3fdc5780a3e0ab0c20c
Ollie Batchelor <olliebatchelor@hotmail.com>
2023-12-20 15:05:01 +0000

    resolve the db migration postgres issues

--- rust-containers-k8s/database/docker-postgresql-multiple-databases/create-multiple-postgresql-databases.sh
+++ rust-containers-k8s/database/docker-postgresql-multiple-databases/create-multiple-postgresql-databases.sh
@@ -9,7 +9,7 @@ function create_user_and_database() {
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
         CREATE USER $database WITH PASSWORD '$POSTGRES_PASSWORD';
         CREATE DATABASE $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+		ALTER DATABASE $database OWNER TO $database;
 EOSQL
 }
 
---
+++ rust-containers-k8s/inventory-service/migrations/20231220142744_seed.up.sql
@@ -0,0 +1,4 @@
+-- Add up migration script here
+INSERT INTO t_inventory
+VALUES (1, 100, 'iphone_13'),
+    (2, 0, 'iphone_13_red');
