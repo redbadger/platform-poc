2489cc82fd8bec3fa5650df9ba47020b306cbb7f
Pedro Martin <pataruco@gmail.com>
2023-12-21 15:35:08 +0000

    Deserialise event payload

--- rust-containers-k8s/notification-service/src/types.rs
+++ rust-containers-k8s/notification-service/src/types.rs
@@ -1,6 +1,8 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+
 pub struct OrderPlacedEvent {
     pub order_number: String,
 }
