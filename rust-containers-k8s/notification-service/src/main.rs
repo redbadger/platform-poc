e7acd812fee3672bfbf2c324762e7e802dde0b57
Pedro Martin <pataruco@gmail.com>
2023-12-21 15:00:14 +0000

    Create kafka consumer

--- rust-containers-k8s/notification-service/src/main.rs
+++ rust-containers-k8s/notification-service/src/main.rs
@@ -1,6 +1,30 @@
-use std::{thread, time::Duration};
+use notification_service::types::OrderPlacedEvent;
+use rdkafka::{
+    consumer::{Consumer, StreamConsumer},
+    ClientConfig, Message,
+};
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     println!("Notification service started");
-    thread::sleep(Duration::from_secs(60 * 60 * 24));
+
+    let consumer = create_consumer("localhost:9092")?;
+
+    consumer.subscribe(&["OrderPlacedEvent"])?;
+
+    loop {
+        let message = consumer.recv().await?;
+        let payload = message.payload().unwrap();
+        let content: OrderPlacedEvent = serde_json::from_slice(payload)?;
+        println!("Received Notification for Order - {}", content.order_number);
+    }
+}
+
+fn create_consumer(bootstrap_server: &str) -> anyhow::Result<StreamConsumer> {
+    let consumer = ClientConfig::new()
+        .set("bootstrap.servers", bootstrap_server)
+        .set("enable.partition.eof", "false")
+        .set("group.id", "notificationId")
+        .create()?;
+    Ok(consumer)
 }
