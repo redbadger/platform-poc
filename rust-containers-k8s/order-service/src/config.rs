924cbc8df99d25ff65d488c98279394f03f58432
Ollie Batchelor <olliebatchelor@hotmail.com>
2023-12-21 13:31:45 +0000

    add sql migration for orders service

--- rust-containers-k8s/order-service/src/config.rs
+++ rust-containers-k8s/order-service/src/config.rs
@@ -3,6 +3,7 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 pub struct Config {
     pub port: u32,
+    pub database_url: String,
 }
 
 impl Config {
--- rust-containers-k8s/order-service/src/main.rs
+++ rust-containers-k8s/order-service/src/main.rs
@@ -1,19 +1,30 @@
 use dotenv::dotenv;
 use order_service::{api::server, config::Config};
 use sqlx::postgres::PgPoolOptions;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "order_service=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     dotenv().ok();
-    let config = Config::new()?;
+    let config = Config::new().expect("Can't read config");
 
     println!("{:?}", config);
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        // TODO: paramterise this
-        .connect("postgres://commerce:commerce@localhost/order-service")
+        .connect(&config.database_url)
         .await?;
+
+    sqlx::migrate!().run(&pool).await?;
 
     server::create(config, pool).await?;
     Ok(())
