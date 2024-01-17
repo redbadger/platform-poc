23b3cce125ee9feb212f010b8237be3da5f5f268
Stuart Harris <stuart.harris@red-badger.com>
2024-01-17 14:42:52 +0000

    kv wit versions

--- rust-wasmcloud/virtual-keyvalue-batch/src/lib.rs
+++ rust-wasmcloud/virtual-keyvalue-batch/src/lib.rs
@@ -13,43 +13,43 @@ use wasi::keyvalue::types::{Bucket, Error, IncomingValue, Key, Keys, OutgoingVal
 
 impl BatchExport for KeyValueAdapter {
     fn get_many(
-        bucket: &Bucket,
+        bucket: Bucket,
         keys: Keys,
     ) -> Result<wit_bindgen::rt::vec::Vec<IncomingValue>, Error> {
         todo!()
     }
 
-    fn get_keys(bucket: &Bucket) -> Keys {
+    fn get_keys(bucket: Bucket) -> Keys {
         todo!()
     }
 
     fn set_many(
-        bucket: &Bucket,
-        key_values: wit_bindgen::rt::vec::Vec<(Key, &OutgoingValue)>,
+        bucket: Bucket,
+        key_values: wit_bindgen::rt::vec::Vec<(Key, OutgoingValue)>,
     ) -> Result<(), Error> {
         todo!()
     }
 
-    fn delete_many(bucket: &Bucket, keys: Keys) -> Result<(), Error> {
+    fn delete_many(bucket: Bucket, keys: Keys) -> Result<(), Error> {
         todo!()
     }
 }
 
 impl ReadWriteExport for KeyValueAdapter {
-    fn get(bucket: &Bucket, key: Key) -> Result<IncomingValue, Error> {
+    fn get(bucket: Bucket, key: Key) -> Result<IncomingValue, Error> {
         todo!()
     }
 
-    fn set(bucket: &Bucket, key: Key, outgoing_value: &OutgoingValue) -> Result<(), Error> {
+    fn set(bucket: Bucket, key: Key, outgoing_value: OutgoingValue) -> Result<(), Error> {
         todo!()
     }
 
-    fn delete(bucket: &Bucket, key: Key) -> Result<(), Error> {
+    fn delete(bucket: Bucket, key: Key) -> Result<(), Error> {
         todo!()
     }
 
     #[doc = " Check if the key exists in the bucket."]
-    fn exists(bucket: &Bucket, key: Key) -> Result<bool, Error> {
+    fn exists(bucket: Bucket, key: Key) -> Result<bool, Error> {
         todo!()
     }
 }
