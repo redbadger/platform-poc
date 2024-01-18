a43262651fa50a0c701698cbd24618d271e3eda6
Viktor Charypar <charypar@gmail.com>
2024-01-18 10:39:54 +0000

    Restructure the WITs into a master copy and local dependencies

---
+++ rust-wasmcloud/platform-wasmcloud/src/lib.rs
@@ -0,0 +1,23 @@
+wit_bindgen::generate!({
+    world: "platform-wasmcloud",
+    exports: {
+        "platform-poc:keyvalue/keyvalue": KeyValueAdapter,
+    }
+});
+
+use exports::platform_poc::keyvalue::keyvalue::Guest as KeyValueExport;
+use exports::platform_poc::keyvalue::keyvalue::{Bucket, Error, Key, KeyValue, Value};
+
+struct KeyValueAdapter;
+
+const SEPARATOR: &str = " ";
+
+impl KeyValueExport for KeyValueAdapter {
+    fn get_all(bucket: Bucket) -> Result<Vec<KeyValue>, Error> {
+        todo!()
+    }
+
+    fn set(bucket: Bucket, key: Key, value: Value) -> Result<(), Error> {
+        todo!()
+    }
+}
