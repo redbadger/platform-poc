d5d002d4cc61d020d700b53625b41dccb03ee409
Viktor Charypar <charypar@gmail.com>
2024-01-19 14:34:05 +0000

    Implement a basic inventory interface

--- rust-wasmcloud/inventory/src/lib.rs
+++ rust-wasmcloud/inventory/src/lib.rs
@@ -5,12 +5,19 @@ wit_bindgen::generate!({
     }
 });
 
-use exports::platform_poc::inventory::inventory::Guest as InventoryExport;
+use exports::platform_poc::inventory::inventory::{Availability, Guest as InventoryExport};
 
 struct InventoryService;
 
 impl InventoryExport for InventoryService {
-    fn hello() -> String {
-        "hello from inventory".to_string()
+    fn get_inventory(sku_code: Vec<i32>) -> Vec<Availability> {
+        // FIXME read from an SQL database
+        sku_code
+            .iter()
+            .map(|sku| Availability {
+                sku_code: *sku,
+                is_in_stock: true,
+            })
+            .collect()
     }
 }
