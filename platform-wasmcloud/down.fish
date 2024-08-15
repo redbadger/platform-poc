414914b9fd0d891cb35237708cb20dcd31a0f03f
Stuart Harris <stuart.harris@red-badger.com>
2024-08-15 11:41:17 +0100

    up/down scripts

--- platform-wasmcloud/down.fish
+++ platform-wasmcloud/down.fish
@@ -1,3 +1,13 @@
 #!/usr/bin/env fish
 
-wash app delete ./wadm.yaml
+wash down --all
+
+pushd /tmp
+    if test -f wash-ui.pid
+        set PID (cat wash-ui.pid)
+        rm -f wash-ui.pid wash-ui.out
+        if test -n "$PID"
+            kill $PID
+        end
+    end
+popd
--- platform-wasmcloud/up.fish
+++ platform-wasmcloud/up.fish
@@ -1,3 +1,11 @@
 #!/usr/bin/env fish
 
-wash app deploy ./wadm.yaml
+wash up -d
+
+pushd /tmp
+    status job-control full
+    nohup wash ui > wash-ui.out &
+    cat wash-ui.out
+    set PID $last_pid
+    echo $PID > wash-ui.pid
+popd
