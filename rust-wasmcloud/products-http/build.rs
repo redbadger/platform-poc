719941985e2afe7bad87735fe761ee60c0e32123
Stuart Harris <stuart.harris@red-badger.com>
2025-02-06 11:19:26 +0000

    remove rust-wasmcloud as now obsolete

--- rust-wasmcloud/products-http/build.rs
+++
@@ -1,8 +0,0 @@
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    println!("cargo:rerun-if-changed=./src");
-    println!("cargo:rerun-if-changed=./Cargo.toml");
-
-    wit_deps::lock!().await.unwrap();
-    Ok(())
-}
